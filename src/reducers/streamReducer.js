d98f632b9913d71f48403efc0b8f5fa205aaa113
SallahTech <DonlyRlMrKtr@gmail.com>
2021-02-11 23:34:10 +0200

    added a json sever to hold all streams to serve as a database, added the action creattors for creating streams, dleting, editing and fetching and all created the streamReducer

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,26 @@
+import _ from 'lodash'
+import {
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from '../actions/types'
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload }
+      break
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload }
+      break
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload }
+      break
+    case DELETE_STREAM:
+      return _.omit(state, action.payload)
+    default:
+      return state
+  }
+}
